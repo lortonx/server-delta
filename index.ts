7c624fd813371cf1ca550e32d768e8de2db29d3d
lortonx <lortonx@gmail.com>
2024-01-10 22:16:51 +0000

    fix

--- index.ts
+++ index.ts
@@ -89,13 +89,13 @@ const config = {
 
 const parseServer = ParseServer(config);
 
-// const parseGraphQLServer = new ParseGraphQLServer(parseServer, {
-//     graphQLPath: '/graphql',
-//     playgroundPath: '/playground',
-//     graphQLCustomTypeDefs: gql`
-//         ${fs.readFileSync('./src/cloud/schema.graphql')}
-//     `
-// });
+const parseGraphQLServer = new ParseGraphQLServer(parseServer, {
+    graphQLPath: '/graphql',
+    playgroundPath: '/playground',
+    graphQLCustomTypeDefs: gql`
+        ${fs.readFileSync('./src/cloud/schema.graphql')}
+    `
+});
 
 /**
  * Parse govnocode fix
@@ -109,7 +109,7 @@ const parseServer = ParseServer(config);
 //         }
 //         return real_resolve.apply(this, arguments);
 //     };
-
+parseServer.start();
 // parseServer.start().then(() => {
 //     import('./Models/Installer');
 // });
